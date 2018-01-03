17b37f0e192136f53733b61be981ad203364180c
EstiYuliani <32305158+EstiYuliani@users.noreply.github.com>
2018-01-03 17:49:48 +0800

    Membuat deklarasi variabel

--- SAW.cpp
+++ SAW.cpp
@@ -2,3 +2,11 @@
 #include <cmath>
 using namespace std;
 
+int main(){
+	// DEKLARASI VARIABEL
+	double a[3][5]={{4,4,5,3,3},{3,3,4,2,3},{5,4,2,2,2}};
+		
+	double w[5]={5,3,4,4,2};
+	double ha1,ha2,ha3;
+	
+}
