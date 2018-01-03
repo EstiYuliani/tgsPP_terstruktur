f1f51ec366b4621a12a7f0e2863915207e543400
EstiYuliani <32305158+EstiYuliani@users.noreply.github.com>
2018-01-03 18:03:00 +0800

    Menampilkan nilai w

--- SAW.cpp
+++ SAW.cpp
@@ -42,6 +42,12 @@ int main(){
 		cout<<endl;
 	}
 	cout<<endl;
-}
 
-	
+	// MENAMPILKAN NILAI W
+	cout << "BOBOT W :"<<endl;
+	for(int i=0;i<5;i++){
+		cout<< w[i] <<" | ";
+	}
+	cout << endl;
+	cout << endl;
+}
